ab1266aa3bf5ea50596145e121431a0be36dca20
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-08-03 14:37:03 +0100

    Plot results in example

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # pid.jl
 
 PID controller implementation in Julia.
+
+# Requirements
+
+* [Plots](https://docs.juliaplots.org/stable/)
--- example.jl
+++ example.jl
@@ -1,3 +1,5 @@
+using Plots
+
 include("pid.jl")
 
 # global process_variable
@@ -12,8 +14,15 @@ n_steps::Int64 = 20
 
 println("x(0) = ", process_variable)
 
+pv = Array{Float64}(undef, n_steps + 1)
+pv[1] = process_variable
+
 for i in 1:n_steps
     global process_variable
     process_variable = PIDControl.next(pid, set_point, process_variable, time_step)
+    pv[i+1] = process_variable
     println("x(", i, ") = ", process_variable)
 end
+
+p = plot(range(1, n_steps+1), [pv, set_point*ones(n_steps+1)], label=["process variable" "set point"])
+display(p)
